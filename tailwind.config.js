089221e03617c55aff04b3ed448b92db6fa53474
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-08-02 02:01:24 -0300

    feat: Add assets (images, SVGs, fonts) to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,11 +8,13 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+
     extend: {
+      fontFamily: {
+        coolvetica: ['var(--font-coolvetica)', ...defaultTheme.fontFamily.sans]
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'city': "url('/img/background.jpg')",
       },
     },
   },
