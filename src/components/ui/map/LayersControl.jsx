b14a5fe7a6c004d92b5cb4092ae30c00f82a9fcf
Pablo Peña <pablo.sebastian.pena@gmail.com>
2023-08-02 16:41:25 -0300

    feat: Add prop to collapse the control

--- src/components/ui/map/LayersControl.jsx
+++ src/components/ui/map/LayersControl.jsx
@@ -30,45 +30,50 @@ const LAYERS = [
 
 const LayersControl = ({
   toggleLayer,
-  layers
+  layers,
+  collapse
 }) => {
   return (
     <div className='bg-white rounded-md py-2 px-4'>
       <h2 className='font-semibold text-center text-lg'>
         Layers
       </h2>
-      <ul className='flex flex-col gap-2 my-4'>
-        {
-          LAYERS.map((layer, index) => (
-            <li
-              className='w-full'
-              key={index}
-            >
-              <button
-                className='relative flex w-full items-center gap-3 py-2 hover:bg-neutral-200 rounded-md'
-                onClick={() => toggleLayer(layer.name)}
-              >
-                {
-                  layers[layer.name] === false && (
-                    <div className='h-[2px] absolute top-1/2 -translate-y-1/2 w-full bg-red-800'>
-                    </div>
-                  )
-                }
-                <Image
-                  src={layer.img}
-                  alt={layer.name}
-                  width={22}
-                  height={22}
-                  className='ml-2'
-                />
-                <span className='text-md leading-none font-semibold text-[#333] mr-2'>
-                  {layer.name}
-                </span>
-              </button>
-            </li>
-          ))
-        }
-      </ul>
+      {
+        !collapse && (
+          <ul className='flex flex-col gap-2 my-4'>
+            {
+              LAYERS.map((layer, index) => (
+                <li
+                  className='w-full'
+                  key={index}
+                >
+                  <button
+                    className='relative flex w-full items-center gap-3 py-2 hover:bg-neutral-200 rounded-md'
+                    onClick={() => toggleLayer(layer.name)}
+                  >
+                    {
+                      layers[layer.name] === false && (
+                        <div className='h-[2px] absolute top-1/2 -translate-y-1/2 w-full bg-red-800'>
+                        </div>
+                      )
+                    }
+                    <Image
+                      src={layer.img}
+                      alt={layer.name}
+                      width={22}
+                      height={22}
+                      className='ml-2'
+                    />
+                    <span className='text-md leading-none font-semibold text-[#333] mr-2'>
+                      {layer.name}
+                    </span>
+                  </button>
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
     </div>
   )
 }
